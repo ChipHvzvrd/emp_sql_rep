3a2a1ca7ffc01886fffe2bddc86fc8d4841d1d82
Christian Pena <Chip@Christians-MBP.attlocal.net>
2022-08-18 15:55:22 -0500

    testing sql server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 
@@ -7,13 +8,43 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Matrix2016!',
+        database: 'mycompany',
+    },
+    console.log('Connected to the mycompany database')
+);
 
+// db.query(`DELETE FROM employee WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+const sql = `INSERT INTO employee (id, first_name, last_name, department, job_title)
+                 VALUES (?,?,?,?,?)`;
+const params = [1, 'Bruce', 'Wayne', 'R&D', 'Entry'];
+
+db.query(sql, params, (err, result) => {
+     if (err){
+         console.log(err);
+     }
+     console.log(result);
+ });
 // app.get('/', (req, res) => {
 //     res.json({
 //         message: "~Server is running~"
 //     });
 // });
-
+// db.query(`SELECT * FROM employee WHERE id=1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
 
 app.use((req, res) => {
     res.status(404).end();
